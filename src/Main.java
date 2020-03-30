e6f12b58e167cf5b8ff82267fb17f8dda9dcc9f1
MiguelPedrosa <pedrosa.miguel@gmail.com>
2020-03-30 18:25:45 +0100

    Removed main from parser into seperate file

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.io.InputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String args[]) throws ParseException {
+
+        final String filePath = args[0];
+        Parser myProgram = new Parser(openFile(filePath));
+
+        SimpleNode root = myProgram.Start();
+        root.dump("");
+    }
+
+    public static InputStream openFile(String filePath) {
+        final File file = new File(filePath);
+        try {
+            return new FileInputStream(file);
+        } catch(FileNotFoundException e) {
+            System.out.println("No file given.");
+            return null;
+        }
+    }
+}
