2de28033ec055d9ddf934ff91d16dcd6a3f30216
MiguelPedrosa <pedrosa.miguel@gmail.com>
2020-03-30 18:25:13 +0100

    Removed hashs of nodes

---
+++ javacc/ASTElse.java
@@ -0,0 +1,11 @@
+public
+class ASTElse extends SimpleNode {
+  public ASTElse(int id) {
+    super(id);
+  }
+
+  public ASTElse(Parser p, int id) {
+    super(p, id);
+  }
+
+}
---
+++ javacc/ASTLength.java
@@ -0,0 +1,11 @@
+public
+class ASTLength extends SimpleNode {
+  public ASTLength(int id) {
+    super(id);
+  }
+
+  public ASTLength(Parser p, int id) {
+    super(p, id);
+  }
+
+}
