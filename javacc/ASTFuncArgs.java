4cdbfd0f0537fcff2ccfecfd17535847e91935f4
MiguelPedrosa <pedrosa.miguel@gmail.com>
2020-04-10 00:57:57 +0100

    Improved length and function call nodes

---
+++ javacc/ASTFuncArgs.java
@@ -0,0 +1,11 @@
+public class ASTFuncArgs extends SimpleNode {
+
+	public ASTFuncArgs(int id) {
+		super(id);
+	}
+
+	public ASTFuncArgs(Parser p, int id) {
+		super(p, id);
+	}
+
+}
---
+++ javacc/ASTFuncCall.java
@@ -0,0 +1,10 @@
+public class ASTFuncCall extends SimpleNode {
+	public ASTFuncCall(int id) {
+		super(id);
+	}
+
+	public ASTFuncCall(Parser p, int id) {
+		super(p, id);
+	}
+
+}
