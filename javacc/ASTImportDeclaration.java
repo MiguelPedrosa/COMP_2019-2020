05f25576b3cf9f92bb7e8dab4d89e23f0f0671c4
MiguelPedrosa <pedrosa.miguel@gmail.com>
2020-04-25 19:35:01 +0100

    Adjusted how information of imports is stored

--- javacc/ASTImportDeclaration.java
+++ javacc/ASTImportDeclaration.java
@@ -4,6 +4,7 @@ import java.util.List;
 public class ASTImportDeclaration extends SimpleNode {
 
 	protected String methodName;
+	protected String className;
 	protected List<String> argumentTypes;
 	protected Boolean isStatic;
 	protected String returnType;
@@ -13,7 +14,8 @@ public class ASTImportDeclaration extends SimpleNode {
 		// By default, class import is non-static
 		this.isStatic = false;
 		// By default, class import is void
-		this.returnType = "void"; 
+		this.returnType = "void";
+		this.className = null;
 		this.methodName = null;
 		this.argumentTypes = new ArrayList<>();
 	}
@@ -22,12 +24,12 @@ public class ASTImportDeclaration extends SimpleNode {
 		super(p, id);
 	}
 
-	public void buildMethodName(String name) {
-		if(this.methodName == null) {
-			this.methodName = name;
-		} else {
-			this.methodName += "." + name;
-		}
+	public void setClass(String className) {
+		this.className = className;
+	}
+
+	public void setMethodName(String methodName) {
+		this.methodName = methodName;
 	}
 
 	public void addArgument(String arg) {
@@ -44,10 +46,11 @@ public class ASTImportDeclaration extends SimpleNode {
 
 	@Override
 	public String toString() {
-		final String name = "name=\'" + methodName + "\'";
+		final String classInfo = "class=\'" + className + "\'";
+		final String methodInfo = methodName != null ? "method=\'" + methodName + "\'" : "constructor";
 		final String staticInfo = (this.isStatic ? "static" : "non-static");
 		final String args = "arguments=" + argumentTypes;
 		final String returnInfo = "return=" + returnType;
-		return super.toString() + " [ " + name + "; " + staticInfo + "; " + args + "; " + returnInfo + " ]";
+		return super.toString() + " [ " + classInfo + "; "+ methodInfo + "; " + staticInfo + "; " + args + "; " + returnInfo + " ]";
 	}
 }
