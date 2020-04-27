a5f33ba8d965e69658dc4e6fbb1ca0dbafa3df63
André Lopes dos Santos <up200505634@fe.up.pt>
2020-04-27 20:39:02 +0100

    WriteMethod Done

--- src/generator/CodeGenerator.java
+++ src/generator/CodeGenerator.java
@@ -3,6 +3,10 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.FileSystem;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -143,6 +147,105 @@ public class CodeGenerator {
         writeInitializer(scope);
 
         readNodes(classNode, scope);
+    }
+
+    /**
+     * Method to transform a type string into a type for Jasmin
+     */
+
+    private String transformType(String type) {
+
+        String typeString;
+
+        switch (type) {
+            case "int":
+                typeString = "I";
+                break;
+            case "long":
+                typeString = "L";
+                break;
+            case "float":
+                typeString = "F";
+                break;
+            case "double":
+                typeString = "D";
+                break;
+            case "byte":
+                typeString = "B";
+                break;
+            case "char":
+                typeString = "D";
+                break;
+            case "short":
+                typeString = "S";
+                break;
+            case "reference":
+                typeString = "A";
+                break;
+            case "String":
+                typeString = "Ljava/lang/String;";
+                break;
+            case "int[]":
+                typeString = "[I";
+                break;
+            case "long[]":
+                typeString = "[L";
+                break;
+            case "float[]":
+                typeString = "[F";
+                break;
+            case "double[]":
+                typeString = "[D";
+                break;
+            case "byte[]":
+                typeString = "[B";
+                break;
+            case "char[]":
+                typeString = "[D";
+                break;
+            case "short[]":
+                typeString = "[S";
+                break;
+            case "reference[]":
+                typeString = "[A";
+                break;
+            case "String[]":
+                typeString = "[Ljava/lang/String;";
+                break;
+
+        }
+
+        return typeString;
+
+    }
+
+    /**
+     * Method to write a Method (or function) into the file
+     */
+    private void writeMethod(ASTmethodDeclaration methodNode, int scope) {
+        System.out.println("Writing function...");
+        String methodName = methodNode.getMethodName();
+        String methodType = transformType(methodNode.getReturnType());
+
+        LinkedHashMap<String, String> arguments = methodNode.getArguments();
+        String argsInJasmin = "";
+
+        Set set = arguments.entrySet();
+
+        Iterator i = set.iterator();
+
+        while (i.hasNext()) {
+            Map.Entry ma = (Map.Entry) i.next();
+            String argType = transformType(ma.getValue().toString());
+            argsInJasmin = argsInJasmin.concat(argType);
+        }
+
+        writeCode(".method public static " + methodName + "(" + argsInJasmin + ")" + methodType, scope);
+
+        readNodes(methodNode, scope);
+
+        endMethod(scope);
+
     }
 
 }
