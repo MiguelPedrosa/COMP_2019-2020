34c98ac1fdef1d15d3df66feb4a5b2e319f17935
MiguelPedrosa <pedrosa.miguel@gmail.com>
2020-06-06 19:25:16 +0100

    Added code to easy debug

--- src/generator/optionR/GraphNode.java
+++ src/generator/optionR/GraphNode.java
@@ -3,20 +3,26 @@ import java.util.List;
 
 public class GraphNode {
 
-    private int var; 
     private List<GraphNode> neighbours; 
     private int color; 
     private Boolean hidden; 
 
-    public GraphNode(int var) {
-        this.var = var;
+    public GraphNode() {
         this.color = -1;
         this.hidden = false;
         this.neighbours = new ArrayList<>();
     }
 
-    public int getVar(){
-        return var;
+    public void addNeighbour(GraphNode newNeightbour) {
+        if(!this.neighbours.contains(newNeightbour)) {
+            this.neighbours.add(newNeightbour);
+        }
     }
 
+    public void printNode() {
+        System.out.printf("Color: %d; Hidden: %s; Neighbours: %s\n",
+            this.color,
+            (hidden ? "true" : "false"),
+            this.neighbours);
+    }
 }
